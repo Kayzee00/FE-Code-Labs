b0f522891b3c8faa6b08cc908afe83e2ae152482
Kayzee00 <140985050+Kayzee00@users.noreply.github.com>
2023-09-06 23:25:30 -0500

    index.html
    created a title for the page

---
+++ JavaScript-Scope/script.js
@@ -0,0 +1,82 @@
+//let game = 'Sonic'; //global variable
+
+//console.log(game);
+
+//function title(){
+  //console.log(`${game}`);
+//}
+
+//title();
+
+//if(true) {
+//  console.log(`${game}`);
+//}
+
+//function a() {
+  //let game ='Sonic'; //local variable
+  //console.log(game);
+//}
+
+//a();
+
+//if(true) {
+  //let game = 'Mario';
+  //console.log(game);
+//}
+
+//console.log(game);
+//if(true) {
+  //console.log(game);
+//}
+
+//funtion hello() {
+  //var greeting = 'hello';
+//}
+
+//console.log(greeting);
+
+//if(true) {
+  //var greeting = 'hello';
+//}
+
+//console.log(greeting);
+
+//if(true) {
+  //let greeting = 'hello';
+//}
+
+//console.log(greeting);
+
+//let x = 10;
+//console.log(x);
+
+//if(true) {
+  //let x = 2;
+  //console.log(x);
+//}
+
+//console.log(x);
+
+//LESSON TASKS//
+
+function user(){
+  let firstName = 'John';
+  let lastName = 'Smith';
+  let age = 25;
+  let country = 'England'
+
+  console.log(`${firstName} ${lastName} is ${age} years old and lives in ${country}`);
+}
+
+let firstName = 'James';
+
+user();
+
+let game = "Sonic";
+
+if (true) {
+  let game = "Mario";
+  console.log(game);
+}
+
+console.log(game);
