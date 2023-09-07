322e8b2798e394436d8e0bb8cfc24faacbaea744
Kayzee00 <140985050+Kayzee00@users.noreply.github.com>
2023-09-07 14:21:00 -0500

    JavaScript-Scoping
    learned global scope
    module scope
    block scope
    function scope
    used multiple variables with same name

---
+++ JavaScript-Scope2/script.js
@@ -0,0 +1,31 @@
+//const a = 1 
+
+//function test() {
+  //const b =2
+  //console.log("Here", a, b)
+//}
+
+//test()
+//console.log(a, b)
+
+//function test() {
+  //const globalVar = "Global"
+  //console.log(globalVar)
+//}
+//test()
+//console.log(globalVar)
+
+//import {exportedVar} from "./moduleScript.js"
+
+//console.log(moduleVar)
+
+function test() {
+  const c = 2
+
+  if (true) {
+    const b = 3
+    console.log(b, c)
+  }
+}
+
+test()
