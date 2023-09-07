be8cb70a35ba9c78b87f871ff51cf3eee2571de9
Kayzee00 <140985050+Kayzee00@users.noreply.github.com>
2023-09-07 16:32:30 -0500

    JavaScript-Hoisting
    Learned variable hoisting
    JS variable lifecycle
    function hoisting
    and did lesson tasks at end of video

---
+++ JavaScript-Hoisting/script.js
@@ -0,0 +1,47 @@
+//hello();
+
+//function hello() {
+  //console.log('hello');
+//}
+
+/* Declaration */
+//let game;
+//console.log(game); // undefined
+
+/* Initialization */
+//game = "Super Mario";
+//console.log(game); // Super Mario
+
+//console.log(typeof num);
+
+
+//var author; 
+
+//console.log(author);
+
+//author = 'C.S. Lewis';
+
+
+//console.log(author);
+
+//let author = 'C.S. Lewis';
+
+
+//lion('Aslan');
+
+//function lion(name) {
+  //console.log(`The lions name is ${name}`);
+//}
+
+
+
+//LESSON TASKS//
+
+//book('The Magicians Nephew');
+
+//function book(title) {
+  //console.log(`The book is called ${title}`);
+//}
+
+let book = 'The Silver Chair';
+console.log(book);
