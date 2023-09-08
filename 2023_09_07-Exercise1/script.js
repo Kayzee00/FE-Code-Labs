a851033e986022c52bdfea4ac2f1e2b43a76baba
Kayzee00 <140985050+Kayzee00@users.noreply.github.com>
2023-09-07 19:19:53 -0500

    JS Scope Exercise#1  9/7/23
    Identified types of scope in context of the code.

---
+++ 2023_09_07-Exercise1/script.js
@@ -0,0 +1,15 @@
+//Given the code snippet. Explain what scopes are in this current context of the code.//
+
+function outerFunction() {
+  let outerVar = "I'm outside!"; // outerVar is local scope
+
+  function innerFunction() {
+    let innerVar = "I'm inside!"; // local scope
+    console.log(outerVar); // Can we access outerVar? yes, parent scope
+    console.log(innerVar); // Can we access innerVar? yes, local scope
+  }
+
+  innerFunction();
+}
+
+outerFunction();
